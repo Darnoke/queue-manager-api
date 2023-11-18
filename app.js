c4ab83e7127cde17a22983ec5a5fe43e4d031761
Konrad <konradwozniak64@gmail.com>
2023-11-18 10:59:32 +0100

    Add .env file and basic session config

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
+const session = require('express-session');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(session({
+  secret: process.env.SECRET_KEY || 'defaultsecret',
+  resave: false,
+  saveUninitialized: false,
+  rolling: true, // auto-refresh
+  cookie: {
+    maxAge: 20 * 60 * 1000, // 20 minutes
+  },  
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
