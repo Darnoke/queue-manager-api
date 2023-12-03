1246e96ef95ee5a18dd8ab52a0bfee00fc1ae45c
Konrad <konradwozniak64@gmail.com>
2023-12-03 12:17:00 +0100

    Added none role

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['client', 'admin'], default: 'client' },
+  role: { type: String, enum: ['client', 'admin', 'none'], default: 'client' },
 });
 
 const User = mongoose.model('User', userSchema);
